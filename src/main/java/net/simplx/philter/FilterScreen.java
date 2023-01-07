45b699aed09708a6fb8407cb4ca8f2a3a865eb0e
Ken Arnold <arnold@peak.im>
2023-01-06 23:51:33 -0800

    make text/font related values in layout looked up at the last minute.

--- src/main/java/net/simplx/philter/FilterScreen.java
+++ src/main/java/net/simplx/philter/FilterScreen.java
@@ -14,6 +14,7 @@ import net.minecraft.client.gui.widget.EditBoxWidget;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
@@ -27,36 +28,23 @@ public class FilterScreen extends HandledScreen<FilterScreenHandler> {
 
   private static final int SCREEN_HEIGHT = 233;
   private static final int TEXT_HEIGHT = MinecraftClient.getInstance().textRenderer.fontHeight + 2;
-  private static final int BUTTON_HEIGHT = TEXT_HEIGHT + 2;
+  private static final int BUTTON_HEIGHT = TEXT_HEIGHT + 4;
   private static final int INVENTORY_BOTTOM = 130;
   private static final int BORDER = 8;
-  private static final int SPACE_WIDTH;
 
-  private static final Text FILTER_TITLE = Text.translatable("philter.filter.name").append(":");
   private static final int FILTER_Y = INVENTORY_BOTTOM + TEXT_HEIGHT / 2;
-  private static final int FILTER_BUTTON_WIDTH;
 
   private static final int MATCHES_LIST_X = BORDER;
   private static final int MATCHES_LIST_Y = FILTER_Y + BUTTON_HEIGHT;
 
-  private static final Text SAVE_TEXT = Text.translatable("philter.save");
   private static final int SAVE_X = BORDER;
   private static final int SAVE_Y = SCREEN_HEIGHT - BORDER - BUTTON_HEIGHT;
-
-  static {
-    var renderer = MinecraftClient.getInstance().textRenderer;
-    var maxWidth = 0;
-    for (FilterMode mode : FilterMode.values()) {
-      maxWidth = Math.max(maxWidth, renderer.getWidth(mode.asString()));
-    }
-    // 2: Space on either side
-    SPACE_WIDTH = renderer.getWidth(" ");
-    FILTER_BUTTON_WIDTH = maxWidth + SPACE_WIDTH * 2;
-  }
 
   private final FilterDesc desc;
   private EditBoxWidget editBox;
   private ButtonWidget saveButton;
+
+  private Text filterTitle;
 
   public FilterScreen(FilterScreenHandler handler, PlayerInventory inventory, Text title) {
     super(handler, inventory, title);
@@ -66,47 +54,53 @@ public class FilterScreen extends HandledScreen<FilterScreenHandler> {
     desc = handler.getFilterDesc();
   }
 
-  @Override
-  protected void drawForeground(MatrixStack matrices, int mouseX, int mouseY) {
-    super.drawForeground(matrices, mouseX, mouseY);
-    textRenderer.draw(matrices, FILTER_TITLE, BORDER,
-        FILTER_Y + (BUTTON_HEIGHT - TEXT_HEIGHT) / 2.0f, TITLE_TEXT_COLOR);
-  }
-
   protected void init() {
     super.init();
-    var textWidth = textRenderer.getWidth(FILTER_TITLE) + textRenderer.getWidth(" ");
+
+    // Calculate the text- and font-relative values.
+    Text saveText = Text.translatable("philter.save");
+    filterTitle = Text.translatable("philter.filter.name").append(":");
+
+    int spaceWidth = textRenderer.getWidth(" ");
+    int textWidth = textRenderer.getWidth(filterTitle) + spaceWidth;
+    int maxWidth = 0;
+    for (FilterMode mode : FilterMode.values()) {
+      maxWidth = Math.max(maxWidth, textRenderer.getWidth(mode.asString()));
+    }
+    int filterButtonWidth = maxWidth + spaceWidth * 2;
+    int saveButtonWidth = textRenderer.getWidth(saveText) + 2 * spaceWidth;
 
     addDrawableChild(
         CyclingButtonWidget.builder(FilterScreen::filterText).values(FilterMode.values())
             .omitKeyText().initially(desc.mode)
-            .build(x + BORDER + textWidth, FILTER_Y, FILTER_BUTTON_WIDTH, BUTTON_HEIGHT, null,
+            .build(x + BORDER + textWidth, FILTER_Y, filterButtonWidth, BUTTON_HEIGHT, null,
                 (button, mode) -> setMode(mode)));
-
-    editBox = addDrawableChild(
-        new EditBoxWidget(client.textRenderer, x + MATCHES_LIST_X, y + MATCHES_LIST_Y,
-            backgroundWidth - 2 * BORDER,
-            SCREEN_HEIGHT - MATCHES_LIST_Y - 2 * BORDER - BUTTON_HEIGHT,
-            Text.literal(desc.matchSpec), Text.literal("message")));
-    editBox.setMaxLength(FilterDesc.MATCHES_MAX_LEN);
-    editBox.setChangeListener(this::updateForSpec);
-
-    int saveWidth =
-        MinecraftClient.getInstance().textRenderer.getWidth(SAVE_TEXT) + 2 * SPACE_WIDTH;
-    saveButton = addDrawableChild(
-        new ButtonWidget.Builder(SAVE_TEXT, this::save).dimensions(x + SAVE_X, y + SAVE_Y,
-            saveWidth, BUTTON_HEIGHT).build());
-
     updateForMode();
+
+    MutableText matchesAltText = Text.translatable("philter.filter_mode.matches_alt");
+    editBox = addDrawableChild(
+        new EditBoxWidget(textRenderer, x + MATCHES_LIST_X, y + MATCHES_LIST_Y,
+            backgroundWidth - 2 * BORDER, SCREEN_HEIGHT - MATCHES_LIST_Y - 2 * BORDER - TEXT_HEIGHT,
+            matchesAltText, matchesAltText));
+    editBox.setMaxLength(FilterDesc.MATCHES_MAX_LEN);
+    editBox.setText(desc.matchSpec);
+    editBox.setChangeListener(this::updateForSpec);
     updateForSpec(desc.matchSpec);
+
+    saveButton = addDrawableChild(
+        new ButtonWidget.Builder(saveText, this::save).dimensions(x + SAVE_X, y + SAVE_Y,
+            saveButtonWidth, BUTTON_HEIGHT).build());
+  }
+
+  @Override
+  protected void drawForeground(MatrixStack matrices, int mouseX, int mouseY) {
+    super.drawForeground(matrices, mouseX, mouseY);
+    textRenderer.draw(matrices, filterTitle, BORDER,
+        FILTER_Y + (BUTTON_HEIGHT - TEXT_HEIGHT) / 2.0f, TITLE_TEXT_COLOR);
   }
 
   private void updateForSpec(String text) {
-    boolean newVis = !text.equals(desc.matchSpec) && editBox.visible;
-    if (newVis != saveButton.visible) {
-      System.out.printf("Save button: %s\n", newVis);
-    }
-    saveButton.visible = newVis;
+    saveButton.visible = !text.equals(desc.matchSpec) && editBox.visible;
   }
 
   private static Text filterText(FilterMode value) {
@@ -120,11 +114,7 @@ public class FilterScreen extends HandledScreen<FilterScreenHandler> {
   }
 
   private void updateForMode() {
-    boolean newVis = desc.mode == MATCHES;
-    if (newVis != editBox.visible) {
-      System.out.printf("Edit Box visible: %s\n", newVis);
-    }
-    editBox.visible = newVis;
+    editBox.visible = desc.mode == MATCHES;
   }
 
   private void storeText() {
