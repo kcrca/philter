11c0e3770d1060c740f1a9a04496c5dc87e2f871
Ken Arnold <arnold@peak.im>
2023-01-12 22:06:30 -0800

    more doc stuff

--- src/main/java/net/simplx/mcguidoc/DocScreen.java
+++ src/main/java/net/simplx/mcguidoc/DocScreen.java
@@ -1,11 +1,22 @@
 package net.simplx.mcguidoc;
 
+import static net.simplx.mcgui.Horizontal.LEFT;
+import static net.simplx.mcgui.Horizontal.RIGHT;
+import static net.simplx.mcgui.Vertical.ABOVE;
+import static net.simplx.mcgui.Vertical.BELOW;
+import static net.simplx.mcgui.Vertical.MID;
+
+import java.util.List;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.widget.CyclingButtonWidget;
+import net.minecraft.client.gui.widget.LockButtonWidget;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.simplx.mcgui.Layout;
+import net.simplx.mcgui.Layout.Placer;
 
 public class DocScreen extends HandledScreen<DocScreenHandler> {
 
@@ -22,6 +33,33 @@ public class DocScreen extends HandledScreen<DocScreenHandler> {
   protected void init() {
     super.init();
     layout = new Layout(this);
+    layout.setPrefix("moodle");
+    Placer p;
+    Text button1Text = layout.text("button1");
+    p = layout.placer().withText(button1Text).x(LEFT).y(ABOVE).inButton();
+    var button1 = addDrawableChild(
+        new ButtonWidget.Builder(button1Text, this::doStuff).dimensions(p.x(), p.y(), p.w(), p.h())
+            .build());
+    Text colorText = layout.text("colors");
+    String[] colors = new String[]{"red", "green", "blue"};
+    p = layout.placer().withTexts(layout.texts(List.of(colors))).x(RIGHT, button1).y(MID, button1)
+        .inButton();
+    var colorButton = addDrawableChild(
+        CyclingButtonWidget.builder(name -> layout.text((String) name))
+            .tooltip(name -> layout.tooltip(name + ".tooltip"))
+            .build(p.x(), p.y(), p.w(), p.h(), null, (button, name) -> setColor(name)));
+    p = layout.placer().lockButton().x(colorButton.getX()).y(BELOW, colorButton);
+    addDrawableChild(new LockButtonWidget(p.x(), p.y(), this::toggleLock));
+  }
+
+  private void toggleLock(ButtonWidget buttonWidget) {
+  }
+
+  private void setColor(Object name) {
+  }
+
+  private void doStuff(ButtonWidget buttonWidget) {
+
   }
 
   @Override
