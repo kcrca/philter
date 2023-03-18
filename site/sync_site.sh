0d5a65f0e25798d831bff033b7d2c5e7eac21db8
Ken Arnold <arnold@peak.im>
2023-03-18 12:37:21 -0700

    don't delete philter on sync

--- site/sync_site.sh
+++ site/sync_site.sh
@@ -3,4 +3,4 @@ set -e
 cd `dirname $0`
 ./favicon.sh
 dst_dir="philter"
-rsync -c -avz --delete --exclude=src --exclude='.??*' --exclude='*'.sh --exclude='?' --exclude='.?' --exclude='favicon.p*' . kcrca_claritypack@ssh.phx.nearlyfreespeech.net:$dst_dir/
+rsync -c -avz --delete --exclude=src --exclude='.??*' --exclude='*'.ai --exclude='*'.sh --exclude='?' --exclude='.?' --exclude='favicon.p*' . kcrca_claritypack@ssh.phx.nearlyfreespeech.net:$dst_dir/
